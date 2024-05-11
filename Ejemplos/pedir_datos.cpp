7b37507658419825c5c39e96dafbc8f50011da7d
JorkaerEC <eythanEC@protonmail.com>
2024-05-11 13:32:12 -0500

    Agregada la carpeta Ejemplos

---
+++ Ejemplos/pedir_datos.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int edad;
+ char sexo[10],nombre[10];
+ float altura;
+cout<<"Ingrese su nombre: "<<endl;
+cin>>nombre;
+cout<<"\nIngrese su edad: "<<endl;
+cin>>edad;
+cout<<"\nIngrese su sexo: "<<endl;
+cin>>sexo;
+cout<<"\nIngrese su altura: "<<endl;
+cin>>altura;
+
+return 0;
+ }
